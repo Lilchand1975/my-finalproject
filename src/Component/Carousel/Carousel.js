182c7f1cc82ff84920e556bed4bfd36c46c92d65
chandra Chhetri <chhetricc@gmail.com>
2018-10-09 16:19:32 -0400

    pushing carousel

--- src/Component/Carousel/Carousel.js
+++ src/Component/Carousel/Carousel.js
@@ -3,7 +3,30 @@ import "./Carousel.css";
 
 class Carousel extends Component {
   render() {
-    return;
+    return (
+      <Carousel>
+        <div>
+          <img src="../Images/esl.jpg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="../Images/esl2.jpg" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="../Images/image.jpg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="../Images/lhosar.jpg" />
+          <p className="legend">Legend 3</p>
+        </div>
+        <div>
+          <img src="assets/3.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    );
   }
 }
 
